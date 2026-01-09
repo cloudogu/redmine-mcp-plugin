6490861c3a53f7f6b595de61dbd61970b6f13f40
Marek Markiewka <marek.markiewka@cloudogu.com>
2026-01-09 15:52:20 +0100

    implement find status id

--- app/controllers/mcp_controller.rb
+++ app/controllers/mcp_controller.rb
@@ -26,7 +26,7 @@ class McpController < ApplicationController
       name: "redmine",
       title: "Redmine MCP Server",
       version: "1.0.0",
-      tools: [MeTool, ListIssuesTool, FindUseridTool, FindProjectidTool, FindTrackeridTool],
+      tools: [MeTool, ListIssuesTool, FindUseridTool, FindProjectidTool, FindTrackeridTool, FindStatusidTool],
       server_context: { controller: self },
       configuration: config,
     )

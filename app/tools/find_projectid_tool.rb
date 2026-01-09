c4fc58ae9cb37e9ced3a0b9d785a3659356c10d2
Marek Markiewka <marek.markiewka@cloudogu.com>
2026-01-09 14:59:22 +0100

    implement find project id

---
+++ app/tools/find_projectid_tool.rb
@@ -0,0 +1,39 @@
+class FindProjectidTool < RedmineTool
+  description "Find the id for a project via its name or identifier"
+  input_schema(
+    properties: {
+      query: { type: "string", description: "A part of the projects name or identifier" },
+    },
+    required: ["query"],
+  )
+
+  class << self 
+    def call(server_context:, query:)
+      query.strip!
+      user = User.current
+
+      projects = Project.where("(LOWER(name) LIKE ? OR LOWER(identifier) LIKE ?) AND id IN (?)",
+        "%#{query.downcase}%", "%#{query.downcase}%", user.visible_project_ids)
+
+      if projects.empty?
+        return MCP::Tool::Response.new([{
+                 type: "text",
+                 text: "Could not find any projects matching '#{query}'.",
+               }], error: true)
+      end
+
+      mappedProjects = projects.map do |project|
+        {
+          id: project.id,
+          name: project.name,
+          identifier: project.identifier,
+        }
+      end
+
+      MCP::Tool::Response.new([{
+        type: "text",
+        text: mappedProjects.to_json,
+      }])
+    end
+  end
+end

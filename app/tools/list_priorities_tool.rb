c2ec6eed8a7eac90e2cf31736a7ce120b19bc28c
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2026-01-15 10:32:19 +0100

    Use enumerations/index template instead of custom mapping

--- app/tools/list_priorities_tool.rb
+++ app/tools/list_priorities_tool.rb
@@ -1,8 +1,7 @@
 class ListPrioritiesTool < RedmineTool
   description "Lists all active issue priorities."
   input_schema(
-    properties: {
-    },
+    properties: {},
     required: [],
   )
 
@@ -10,19 +9,11 @@ class ListPrioritiesTool < RedmineTool
     def call(server_context:)
       priorities = IssuePriority.active.sorted
 
-      if priorities.empty?
-        return error_response("Could not find any priorities.")
-      end
-
-      formatted_priorities = priorities.map do |p|
-        {
-          id: p.id,
-          name: p.name,
-          is_default: p.is_default
-        }
-      end
-
-      text_response(formatted_priorities.to_json)
+      json = render_template(server_context, "enumerations/index", {
+        klass: IssuePriority,
+        enumerations: priorities,
+      })
+      text_response(json)
     end
   end
 end
